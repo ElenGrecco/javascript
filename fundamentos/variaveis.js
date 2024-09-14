645535154824ac370195a964a1e8b484a42b18d1
elen_grecco@hotmail.com <elen.fgrecco@senacsp.edu.br>
2024-09-13 21:42:20 -0300

    estudo das variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,25 @@
+/**
+ * Estudo do constante e variáveis
+ * @author Elen Grecco
+ */
+  
+console.clear()
+ 
+//variáveis
+let nome
+let idade
+let vip
+  
+
+nome = "Elen Grecco"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 40
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
+ 
