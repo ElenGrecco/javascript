c61f5d24c7de7c4d5afae406d2238f80eb1d4b76
Elen Grecco <elen_grecco@hotmail.com>
2024-09-19 22:09:57 -0300

    Ficha do aluno

---
+++ exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de um aluno de acdemia
+ * @author Elen Grecco
+ */
+
+//variáveis 
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+
+//entrada de dados
+nome = "Elen Grecco"
+idade = 40
+peso = 62
+altura = 1.59
+vip = true 
+
+//saída
+console.log("Ficha do aluno")
+console.log("______________________________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`Vip: ${vip}`)
